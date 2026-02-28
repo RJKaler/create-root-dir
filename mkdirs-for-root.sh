d26fcd8570f56a188a01db15b47425b23867098c
R.J. Kaler <31756802+RJKaler@users.noreply.github.com>
2026-02-28 03:00:30 -0500

    Update mkdirs-for-root.sh

--- mkdirs-for-root.sh
+++ mkdirs-for-root.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-#This is a basic script that will create a root file system on Ubuntu or other distros. To make this work, you first have to enable root. You can do that simply with sudo passwd root. Root will come with default sys files like /etc/ /sys /mnt 
+#This is a basic script that will create user friendly directories for Ubuntu or other distros (these are the same directories that come as default for regular users). To make this work, you first have to enable root. You can do that simply with sudo passwd root. Root will come with default sys files like /etc/ /sys /mnt 
 #However, beyond that user root lacks the basic dirs of ~/Desktop, ~/Documents ~/Pictures. There are reasons why Ubuntu doesn't create those dirs by default but I won't get into that. I also won't get into the nuances of the discrepencies in 
 #security posture between various distros. I agree with the Debian team, hence I offer this script. I hope it helps! 
 
