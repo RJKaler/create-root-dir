8c4dc1a96eeacccaf833c5cba59284010df1e6d9
R.J. Kaler <31756802+RJKaler@users.noreply.github.com>
2026-02-28 02:55:57 -0500

    Create mkdirs-for-root.sh
    See the comments. I hope this helps!

---
+++ mkdirs-for-root.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+#This is a basic script that will create a root file system on Ubuntu or other distros. To make this work, you first have to enable root. You can do that simply with sudo passwd root. Root will come with default sys files like /etc/ /sys /mnt 
+#However, beyond that user root lacks the basic dirs of ~/Desktop, ~/Documents ~/Pictures. There are reasons why Ubuntu doesn't create those dirs by default but I won't get into that. I also won't get into the nuances of the discrepencies in 
+#security posture between various distros. I agree with the Debian team, hence I offer this script. I hope it helps! 
+
+dircreate() {
+    sudo mkdir -vp /root/{Desktop,Documents,Pictures,Music,Videos} &&
+    sudo chmod 700 /root/{Desktop,Documents,Pictures,Music,Videos} &&
+    echo "Successfully changed permissions for root dirs"
+}
+
+if dircreate; then
+    echo "Finished creating dir system for user root."
+else
+    echo "Error!"
+    exit 1
+fi
